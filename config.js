23d1345f47b2da39e5bc4f84c0e290ebd271217b
CDiGallo <86839683+CDiGallo@users.noreply.github.com>
2023-08-08 11:00:03 +0200

    Create config.js

---
+++ config.js
@@ -0,0 +1,26 @@
+var respecConfig = {
+    specStatus: "unofficial",
+    shortName: 'siard-link',
+    edDraftURI: "https://siard.link/",
+    latestVersion: null,
+    maxTocLevel: 4,
+    postProcess: [jumpTo],
+    github: {
+      repoURL: "tbd",
+      branch: "main", // alternative branch
+    },
+    "authors": [
+	{
+      "name": "Tobias Wildi",
+      "company": "FHGR"
+    } 
+	],
+	"editors": [
+    {
+      "name": "Silvan Auf der Maur",
+      "company": "SFA"
+    }
+  ],
+  };
+
+module.exports = respecConfig
